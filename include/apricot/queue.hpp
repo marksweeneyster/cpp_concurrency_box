eb982bdc3e55b432fa8a0a32619f02054da65a44
marksweeneyster <marksweeneyster@gmail.com>
2024-12-23 18:04:13 -0700

    one more gcc copy-elision

--- include/apricot/queue.hpp
+++ include/apricot/queue.hpp
@@ -422,8 +422,7 @@ namespace apricot {
       // If I move the body of "wait_for_data" here then thread-sanitizer complains (warns) about:
       // 1. double lock of a mutex (.build/apps/demo3_tsan+0x9e80) in __gthread_mutex_lock(pthread_mutex_t*)a
       // 2. data race (.build/apps/demo3_tsan+0x117f5) in std::__uniq_ptr_impl<apricot::Queue3<std::future<int> >::Node, std::default_delete<apricot::Queue3<std::future<int> >::Node> >::_M_ptr() const
-      std::unique_lock<std::mutex> head_lock(
-              std::move(wait_for_data(is_empty)));
+      std::unique_lock<std::mutex> head_lock(wait_for_data(is_empty));
 
       if (!is_empty) {
         value = std::move(*head->data);
