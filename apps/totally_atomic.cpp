457b218d6c4d3bbf11af419f83281a038b7a2767
Mark Sweeney <mark.sweeney@qsc.com>
2025-01-17 16:16:24 -0700

    commented out use of the experiment

--- apps/totally_atomic.cpp
+++ apps/totally_atomic.cpp
@@ -7,7 +7,8 @@ int main() {
   std::mt19937 rng(std::random_device{}());
   std::uniform_int_distribution<int> dist(1, 1000);
 
-  totally_atomic::LockFreeStack<int> lock_free_stack;
+  totally_atomic::lock_free_stack<int> lock_free_stack;
+  //totally_atomic::LockFreeStack<int> lock_free_stack;
 
   for (int ii = 0; ii < 100; ++ii) {
     std::thread t([&]() {
@@ -18,17 +19,18 @@ int main() {
     t.detach();
   }
 
-  int val = -1;
-  lock_free_stack.pop(val);
-  //auto val = lock_free_stack.pop();
+  //int val = -1;
+  //lock_free_stack.pop(val);
+  auto val = lock_free_stack.pop();
 
   unsigned int sz = 100;
   std::vector<std::thread> tvec;
   tvec.reserve(sz);
   for (auto ii = 0U; ii < sz; ++ii) {
     tvec.emplace_back([&lock_free_stack]() {
-      int val = -1;
-      while (lock_free_stack.pop(val)) {}
+      while (lock_free_stack.pop()) {}
+      //int valz = -1;
+      //while (lock_free_stack.pop(valz)) {}
     });
   }
 
@@ -36,6 +38,6 @@ int main() {
     popper.join();
   }
 
-  return val;
-  //return val ? *val : -1;
+  return val ? *val : -1;
+  //return val;
 }
